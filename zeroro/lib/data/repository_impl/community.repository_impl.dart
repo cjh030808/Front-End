5f3390b3e2ad5999f99c5d63b9c1054c417f9435
mousepotato03 <seongjin2@kau.kr>
2025-07-09 02:47:30 +0900

    Feat: Repository 초기 구현, 백엔드 응답&요청 구조 변경에 따라 수정할 듯

---
+++ zeroro/lib/data/repository_impl/community.repository_impl.dart
@@ -0,0 +1,86 @@
+import '../../domain/model/comment/comment.model.dart';
+import '../../domain/model/post/post.model.dart';
+import '../../domain/repository/community.repository.dart';
+import '../data_source/community.api.dart';
+
+class CommunityRepositoryImpl implements CommunityRepository {
+  final CommunityApi _api;
+
+  CommunityRepositoryImpl(this._api);
+
+  @override
+  Future<List<Post>> getPosts(int offset) async {
+    try {
+      return await _api.getPosts(offset);
+    } catch (e) {
+      throw Exception('게시글을 불러오는 중 오류가 발생했습니다: $e');
+    }
+  }
+
+  @override
+  Future<Post> createPost(Post post) async {
+    try {
+      return await _api.createPost(post);
+    } catch (e) {
+      throw Exception('게시글 작성 중 오류가 발생했습니다: $e');
+    }
+  }
+
+  @override
+  Future<Post> updatePost(int id, Post post) async {
+    try {
+      return await _api.updatePost(id, post);
+    } catch (e) {
+      throw Exception('게시글 수정 중 오류가 발생했습니다: $e');
+    }
+  }
+
+  @override
+  Future<void> deletePost(int id) async {
+    try {
+      await _api.deletePost(id);
+    } catch (e) {
+      throw Exception('게시글 삭제 중 오류가 발생했습니다: $e');
+    }
+  }
+
+  @override
+  Future<List<Comment>> getComments(int postId) async {
+    try {
+      return await _api.getComments(postId);
+    } catch (e) {
+      throw Exception('댓글을 불러오는 중 오류가 발생했습니다: $e');
+    }
+  }
+
+  @override
+  Future<Comment> createComment(int postId, Comment comment) async {
+    try {
+      return await _api.createComment(postId, comment);
+    } catch (e) {
+      throw Exception('댓글 작성 중 오류가 발생했습니다: $e');
+    }
+  }
+
+  @override
+  Future<Comment> updateComment(
+    int postId,
+    int commentId,
+    Comment comment,
+  ) async {
+    try {
+      return await _api.updateComment(postId, commentId, comment);
+    } catch (e) {
+      throw Exception('댓글 수정 중 오류가 발생했습니다: $e');
+    }
+  }
+
+  @override
+  Future<void> deleteComment(int postId, int commentId) async {
+    try {
+      await _api.deleteComment(postId, commentId);
+    } catch (e) {
+      throw Exception('댓글 삭제 중 오류가 발생했습니다: $e');
+    }
+  }
+}
---
+++ zeroro/lib/domain/repository/community.repository.dart
@@ -0,0 +1,16 @@
+import '../model/comment/comment.model.dart';
+import '../model/post/post.model.dart';
+
+abstract class CommunityRepository {
+  // 게시글 관련 메서드
+  Future<List<Post>> getPosts(int offset);
+  Future<Post> createPost(Post post);
+  Future<Post> updatePost(int id, Post post);
+  Future<void> deletePost(int id);
+
+  // 댓글 관련 메서드
+  Future<List<Comment>> getComments(int postId);
+  Future<Comment> createComment(int postId, Comment comment);
+  Future<Comment> updateComment(int postId, int commentId, Comment comment);
+  Future<void> deleteComment(int postId, int commentId);
+}
