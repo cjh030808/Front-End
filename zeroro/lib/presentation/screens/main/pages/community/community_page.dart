1c22735c41a6cee1fd05aa4950f4337b7d1c96d0
mousepotato03 <seongjin2@kau.kr>
2025-07-08 16:07:32 +0900

    refactor: 커뮤니티 페이지 스크롤 시, 앱바도 같이 올라감.

--- zeroro/lib/presentation/screens/main/pages/community/community_page.dart
+++ zeroro/lib/presentation/screens/main/pages/community/community_page.dart
@@ -3,48 +3,52 @@ import 'package:go_router/go_router.dart';
 import 'package:zeroro/presentation/routes/route_path.dart';
 import 'commponents/post_widget.dart';
 
+final mockData = [
+  PostWidget(
+    userName: '사용자1',
+    content: '이것은 첫 번째 게시글입니다.',
+    mediaUrls: ['assets/images/mock_image.jpg'],
+  ),
+  SizedBox(height: 16),
+  PostWidget(
+    userName: '사용자2',
+    content: '이것은 두 번째 게시글입니다. 이미지가 여러 장 있어요.',
+    mediaUrls: [
+      'assets/images/mock_image.jpg',
+      'assets/images/mock_image.jpg',
+    ],
+  ),
+  SizedBox(height: 16),
+  PostWidget(
+    userName: '사용자3',
+    content: '세 번째 게시글. 텍스트만 있음.',
+    mediaUrls: [],
+  ),
+];
+
 class CommunityPage extends StatelessWidget {
   const CommunityPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0,
-        title: const Text(
-          'Community',
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-            color: Colors.black,
-          ),
-        ),
-        centerTitle: true,
-      ),
       body: ListView(
-        padding: const EdgeInsets.all(16),
-        children: const [
-          PostWidget(
-            userName: '사용자1',
-            content: '이것은 첫 번째 게시글입니다.',
-            mediaUrls: ['assets/images/mock_image.jpg'],
+        padding: const EdgeInsets.only(left: 16,right: 16,bottom: 16),
+        children:[
+          AppBar(
+            backgroundColor: Colors.white,
+            elevation: 0,
+            title: const Text(
+              'Community',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+            centerTitle: true,
           ),
-          SizedBox(height: 16),
-          PostWidget(
-            userName: '사용자2',
-            content: '이것은 두 번째 게시글입니다. 이미지가 여러 장 있어요.',
-            mediaUrls: [
-              'assets/images/mock_image.jpg',
-              'assets/images/mock_image.jpg',
-            ],
-          ),
-          SizedBox(height: 16),
-          PostWidget(
-            userName: '사용자3',
-            content: '세 번째 게시글. 텍스트만 있음.',
-            mediaUrls: [],
-          ),
+          ...mockData,
         ],
       ),
       floatingActionButton: FloatingActionButton(
